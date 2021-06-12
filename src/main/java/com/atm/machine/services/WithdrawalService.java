853df04a99ec287f5f2f491a8b7418a9a13727af
siddiq <siddiqhamed2017@gmail.com>
2021-06-12 14:34:11 +0200

    updating tests

--- src/main/java/com/atm/machine/services/WithdrawalService.java
+++ src/main/java/com/atm/machine/services/WithdrawalService.java
@@ -1,5 +1,7 @@
 package com.atm.machine.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +10,7 @@ import com.atm.machine.exceptions.CommonServiceException;
 import com.atm.machine.models.Account;
 import com.atm.machine.models.BankNotes;
 import com.atm.machine.repositories.AccountRepository;
+import com.atm.machine.repositories.BankNotesRepository;
 import com.atm.machine.requests.WithdrawalRequest;
 import com.atm.machine.responses.WithdrawalResponse;
 
@@ -16,12 +19,17 @@ public class WithdrawalService {
 
 	@Autowired
 	private AccountRepository accountRepository;
-	
+
 	@Autowired
 	private AccountService accountService;
 
 	@Autowired
-	private DispenseService dispenseService;
+	private BankNotesRepository notesRepository;
+
+	public BankNotes edit(BankNotes bankNotes) throws CommonServiceException {
+
+		return notesRepository.saveAndFlush(bankNotes);
+	}
 
 	@Transactional
 	public WithdrawalResponse withdraw(WithdrawalRequest request) throws CommonServiceException {
@@ -43,18 +51,18 @@ public class WithdrawalService {
 				// if user has access to enough money to cover the withdrawal transaction
 				if (request.getAmount() <= accessibleAmount) {
 					// dispense money
-					BankNotes bankNotes = dispenseService.dispense(request.getAmount());
+					BankNotes bankNotes = dispense(request.getAmount());
 					// successfully dispensed
 					if (bankNotes.getResponseCode().equalsIgnoreCase("0")) {
 						// reflect that on account
 						if (request.getAmount() < retrievedAccount.getBalance()) {
 							retrievedAccount.setBalance(retrievedAccount.getBalance() - request.getAmount());
-						}
-						else if (request.getAmount() > retrievedAccount.getBalance() && request.getAmount() <= accessibleAmount) {
+						} else if (request.getAmount() > retrievedAccount.getBalance()
+								&& request.getAmount() <= accessibleAmount) {
 							retrievedAccount.setBalance(0);
 							retrievedAccount.setOverdraft(accessibleAmount - request.getAmount());
 						}
-						
+
 						Account edited = accountService.edit(retrievedAccount);
 						response.setBankNotes(bankNotes);
 						response.setAccountNumber(edited.getAccountNumber());
@@ -65,9 +73,7 @@ public class WithdrawalService {
 						response.setResponseCode("0");
 						response.setResponseMessage("successful withdrawal and dispense banknotes");
 						response.setResponseStatus("approved");
-					}
-					else
-					{
+					} else {
 						// problem while dispensing notes
 						response.setAccountNumber(request.getAccountNumber());
 						response.setAmount(request.getAmount());
@@ -79,10 +85,8 @@ public class WithdrawalService {
 						response.setResponseMessage(bankNotes.getResponseMessage());
 						response.setResponseStatus(bankNotes.getResponseStatus());
 					}
-					
-				}
-				else
-				{
+
+				} else {
 					// withdrawal amount not valid
 					response.setAccountNumber(request.getAccountNumber());
 					response.setAmount(request.getAmount());
@@ -94,8 +98,7 @@ public class WithdrawalService {
 					response.setResponseMessage("amount is larger than maximum withdrawal amount");
 					response.setResponseStatus("failed");
 				}
-				
-				
+
 			} else {
 
 				response.setAccountNumber(request.getAccountNumber());
@@ -109,6 +112,134 @@ public class WithdrawalService {
 
 		}
 		return response;
+	}
+
+	public BankNotes dispense(double amount) throws CommonServiceException {
+		List<BankNotes> availableNotes = notesRepository.findAll();
+		BankNotes notes;
+		BankNotes notesToBeDispensed = new BankNotes();
+		double result;
+		int remainder;
+		if (amount < 5) {
+			notesToBeDispensed.setFifty(0);
+			notesToBeDispensed.setTwenty(0);
+			notesToBeDispensed.setTen(0);
+			notesToBeDispensed.setFive(0);
+			notesToBeDispensed.setResponseCode("6");
+			notesToBeDispensed.setResponseMessage("can't dispense amount less than 5 euros");
+			notesToBeDispensed.setResponseStatus("failed");
+			return notesToBeDispensed;
+		}
+		if (amount % 5 != 0) {
+			notesToBeDispensed.setFifty(0);
+			notesToBeDispensed.setTwenty(0);
+			notesToBeDispensed.setTen(0);
+			notesToBeDispensed.setFive(0);
+			notesToBeDispensed.setResponseCode("7");
+			notesToBeDispensed.setResponseMessage("can't dispense amount not divisible by 5");
+			notesToBeDispensed.setResponseStatus("failed");
+			return notesToBeDispensed;
+		}
+		if (availableNotes != null) {
+			if (!availableNotes.isEmpty()) {
+				notes = availableNotes.get(0);
+
+				// select the minimum number of notes for the withdrawal
+				int[] dispensibleNotes = { 50, 20, 10, 5 };
+				for (int i = 0; i < dispensibleNotes.length; i++) {
+					if (amount == 0) {
+						break;
+					}
+
+					if (amount < dispensibleNotes[i]) {
+						continue;
+					} else {
+
+						int resdue;
+						double fraction;
+						switch (i) {
+						case 0:
+							result = amount / dispensibleNotes[0];
+							if (result > notes.getFifty()) {
+
+								amount = Math.abs(result - notes.getFifty()) * dispensibleNotes[0];
+								result = notes.getFifty();
+							} else {
+								amount = 0;
+							}
+							resdue = (int) result;
+							fraction = result - resdue;
+							if (fraction != 0) {
+								amount = fraction * dispensibleNotes[0];
+							}
+							notesToBeDispensed.setFifty(resdue);
+
+							break;
+						case 1:
+							result = amount / dispensibleNotes[1];
+							if (result > notes.getTwenty()) {
+
+								amount = Math.abs(result - notes.getTwenty()) * dispensibleNotes[1];
+								result = notes.getTwenty();
+							} else {
+								amount = 0;
+							}
+							resdue = (int) result;
+							fraction = result - resdue;
+							if (fraction != 0) {
+								amount = fraction * dispensibleNotes[1];
+							}
+							notesToBeDispensed.setTwenty(resdue);
+							break;
+						case 2:
+							result = amount / dispensibleNotes[2];
+							if (result > notes.getTen()) {
+								amount = Math.abs(result - notes.getTen()) * dispensibleNotes[2];
+								result = notes.getTen();
+							} else {
+								amount = 0;
+							}
+							resdue = (int) result;
+							fraction = result - resdue;
+							if (fraction != 0) {
+								amount = fraction * dispensibleNotes[2];
+							}
+							notesToBeDispensed.setTen(resdue);
+							break;
+						case 3:
+							result = amount / dispensibleNotes[3];
+							if (result > notes.getFive()) {
+								amount = Math.abs(result - notes.getTen()) * dispensibleNotes[3];
+								result = notes.getFive();
+							}
+							resdue = (int) result;
+							fraction = result - resdue;
+							if (fraction != 0) {
+								amount = fraction * dispensibleNotes[3];
+							}
+							notesToBeDispensed.setFive(resdue);
+							break;
+
+						default:
+							break;
+						}
+					}
+				}
+				// edit the number of banknotes in the reserve
+				notes.setFifty(notes.getFifty() - notesToBeDispensed.getFifty());
+				notes.setTwenty(notes.getTwenty() - notesToBeDispensed.getTwenty());
+				notes.setTen(notes.getTen() - notesToBeDispensed.getTen());
+				notes.setFive(notes.getFive() - notesToBeDispensed.getFive());
+				BankNotes editedBankNotes = edit(notes);
+				notesToBeDispensed.setResponseCode("0");
+				notesToBeDispensed.setResponseMessage("successful dispensing");
+				notesToBeDispensed.setResponseStatus("approved");
+
+			}
+
+		}
+
+		return notesToBeDispensed;
 	}
 
 }
--- src/test/java/com/atm/machine/tests/WithdrawalServiceTest.java
+++ src/test/java/com/atm/machine/tests/WithdrawalServiceTest.java
@@ -1,9 +1,9 @@
 package com.atm.machine.tests;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
+import static org.hamcrest.CoreMatchers.any;
 import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,8 +13,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.atm.machine.exceptions.CommonServiceException;
 import com.atm.machine.models.Account;
@@ -22,10 +22,8 @@ import com.atm.machine.models.BankNotes;
 import com.atm.machine.repositories.AccountRepository;
 import com.atm.machine.repositories.BankNotesRepository;
 import com.atm.machine.requests.WithdrawalRequest;
-import com.atm.machine.responses.AccountResponse;
 import com.atm.machine.responses.WithdrawalResponse;
 import com.atm.machine.services.AccountService;
-import com.atm.machine.services.DispenseService;
 import com.atm.machine.services.WithdrawalService;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,8 +35,6 @@ public class WithdrawalServiceTest {
 	@Mock
 	private AccountService accountService;
 
-	@Mock
-	private DispenseService dispenseService;
 
 	@InjectMocks
 	private WithdrawalService withdrawalService;
@@ -69,6 +65,7 @@ public class WithdrawalServiceTest {
 
 		List<BankNotes> list = new ArrayList<BankNotes>();
 		list.add(initNotes);
+		
 
 		lenient().when(accountRepository.findByAccountNumber("123456789")).thenReturn(one);
 		lenient().when(accountRepository.findByAccountNumber("987654321")).thenReturn(two);
@@ -102,7 +99,7 @@ public class WithdrawalServiceTest {
 
 		WithdrawalRequest request = new WithdrawalRequest();
 		request.setAccountNumber("123456789");
-		request.setPin("1234");
+		request.setPin("12");
 
 		try {
 			WithdrawalResponse recieved = withdrawalService.withdraw(request);
@@ -115,5 +112,257 @@ public class WithdrawalServiceTest {
 		}
 
 	}
+	
+	@Test
+	@DisplayName("Test failed withdrawal for Amount larger than maximum allowed amount")
+	void largerThanAccessibleAmount() {
+
+		WithdrawalRequest request = new WithdrawalRequest();
+		request.setAccountNumber("123456789");
+		request.setPin("1234");
+		request.setAmount(5000);
+
+		try {
+			WithdrawalResponse recieved = withdrawalService.withdraw(request);
+			assertThat(recieved.getResponseCode()).isEqualTo("3");
+			assertThat(recieved.getResponseMessage()).isEqualTo("amount is larger than maximum withdrawal amount");
+			assertThat(recieved.getResponseStatus()).isEqualTo("failed");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test failed withdrawal for amount not dispensible")
+	void failedWithdrawalForBadAmount() {
+
+		WithdrawalRequest request = new WithdrawalRequest();
+		request.setAccountNumber("123456789");
+		request.setPin("1234");
+		request.setAmount(3);
+
+		try {
+			WithdrawalResponse recieved = withdrawalService.withdraw(request);
+			assertThat(recieved.getResponseCode()).isNotEqualTo("0");
+			assertThat(recieved.getResponseStatus()).isEqualTo("failed");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test failed dispense amount less than five")
+	void ShouldNotDispenseWhenAmountLessThanFive() {
+
+		double amount = 3.0 ;
+		BankNotes notes;
+
+		try {
+			notes = withdrawalService.dispense(amount);
+			assertThat(notes.getResponseCode()).isEqualTo("6");
+			assertThat(notes.getResponseMessage()).isEqualTo("can't dispense amount less than 5 euros");
+			assertThat(notes.getResponseStatus()).isEqualTo("failed");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test failed dispense amount not divisible by five")
+	void ShouldNotDispenseWhenAmoutNotDivisibleByFive() {
+
+		double amount = 7 ;
+		BankNotes notes;
+
+		try {
+			notes = withdrawalService.dispense(amount);
+			assertThat(notes.getResponseCode()).isEqualTo("7");
+			assertThat(notes.getResponseMessage()).isEqualTo("can't dispense amount not divisible by 5");
+			assertThat(notes.getResponseStatus()).isEqualTo("failed");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test successful dispensing amount with all notes")
+	void successfulDispensingAllNotes() {
+
+		double amount = 1305.0 ;
+		BankNotes notes;
+
+		try {
+			notes = withdrawalService.dispense(amount);
+			assertThat(notes.getResponseCode()).isEqualTo("0");
+			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
+			assertThat(notes.getResponseStatus()).isEqualTo("approved");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test failed when amount equals zero")
+	void shouldNotDispenseWhenAmountEqualsZero() {
+
+		double amount = 0.0 ;
+		BankNotes notes;
+
+		try {
+			notes = withdrawalService.dispense(amount);
+			assertThat(notes.getResponseCode()).isEqualTo("6");
+			assertThat(notes.getResponseMessage()).isEqualTo("can't dispense amount less than 5 euros");
+			assertThat(notes.getResponseStatus()).isEqualTo("failed");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test successful dispensing less than number of 50 euros notes in the machine")
+	void shouldDispenseSuccessfulLessThan50() {
+
+		double amount = 200.0 ;
+		BankNotes notes;
+
+		try {
+			notes = withdrawalService.dispense(amount);
+			assertThat(notes.getResponseCode()).isEqualTo("0");
+			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
+			assertThat(notes.getResponseStatus()).isEqualTo("approved");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test successful dispensing less than number of 20 euros notes in the machine")
+	void shouldDispenseSuccessfulLessThan20() {
+
+		double amount = 40.0 ;
+		BankNotes notes;
+
+		try {
+			notes = withdrawalService.dispense(amount);
+			assertThat(notes.getResponseCode()).isEqualTo("0");
+			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
+			assertThat(notes.getResponseStatus()).isEqualTo("approved");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test successful dispensing less than number of 10 euros notes in the machine")
+	void shouldDispenseSuccessfulLessThan10() {
+
+		double amount = 10.0 ;
+		BankNotes notes;
+
+		try {
+			notes = withdrawalService.dispense(amount);
+			assertThat(notes.getResponseCode()).isEqualTo("0");
+			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
+			assertThat(notes.getResponseStatus()).isEqualTo("approved");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test successful dispensing less than number of 5 euros notes in the machine")
+	void shouldDispenseSuccessfulLessThan5() {
+
+		double amount = 5.0 ;
+		BankNotes notes;
+
+		try {
+			notes = withdrawalService.dispense(amount);
+			assertThat(notes.getResponseCode()).isEqualTo("0");
+			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
+			assertThat(notes.getResponseStatus()).isEqualTo("approved");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test successful dispensing between 50 and 20")
+	void shouldDispenseSuccessfulBetween50and20() {
+
+		double amount = 1500.0 ;
+		BankNotes notes;
+
+		try {
+			notes = withdrawalService.dispense(amount);
+			assertThat(notes.getResponseCode()).isEqualTo("0");
+			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
+			assertThat(notes.getResponseStatus()).isEqualTo("approved");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test successful dispensing between 10 and 5")
+	void shouldDispenseSuccessfulBetween10and5() {
+
+		double amount = 1700.0 ;
+		BankNotes notes;
+
+		try {
+			notes = withdrawalService.dispense(amount);
+			assertThat(notes.getResponseCode()).isEqualTo("0");
+			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
+			assertThat(notes.getResponseStatus()).isEqualTo("approved");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	@DisplayName("Test failed no banknotes")
+	void shouldNotDispenseMachineIsEmpty() {
+
+		double amount = 1500.0 ;
+		BankNotes notes;
+
+		try {
+			notes = withdrawalService.dispense(amount);
+			notes = withdrawalService.dispense(amount); // repeat
+			assertThat(notes.getResponseCode()).isEqualTo("0");
+			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
+			assertThat(notes.getResponseStatus()).isEqualTo("approved");
+		} catch (CommonServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 
 }
