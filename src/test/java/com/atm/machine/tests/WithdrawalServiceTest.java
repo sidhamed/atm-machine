1624ae203a557414168dec5b0999982c5667a2c7
sidhamed <64863838+sidhamed@users.noreply.github.com>
2021-06-14 02:47:39 +0200

    Update WithdrawalServiceTest.java

--- src/test/java/com/atm/machine/tests/WithdrawalServiceTest.java
+++ src/test/java/com/atm/machine/tests/WithdrawalServiceTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.any;
 import static org.mockito.Mockito.lenient;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -32,15 +33,11 @@ public class WithdrawalServiceTest {
 	@Mock
 	private AccountRepository accountRepository;
 
-	@Mock
-	private AccountService accountService;
-
-
 	@InjectMocks
 	private WithdrawalService withdrawalService;
 
 	@Mock
-	private BankNotesRepository bankNotesRepositotry;
+	private BankNotesRepository bankNotesRepository;
 
 	@BeforeEach
 	public void init() {
@@ -65,11 +62,35 @@ public class WithdrawalServiceTest {
 
 		List<BankNotes> list = new ArrayList<BankNotes>();
 		list.add(initNotes);
+
+		BankNotes notes200 = new BankNotes();
+		notes200.setFifty(4);
+		notes200.setResponseCode("0");
 		
+		BankNotes notes900 = new BankNotes();
+		notes900.setFifty(8);
+		notes900.setResponseCode("0");
+
+		Account account = new Account();
+		account.setAccountNumber("123456789");
+		account.setPin("1234");
+		account.setBalance(600);
+		account.setOverdraft(200);
+		account.setStatus("Active");
+		account.setDateCreated(new Date());
+		account.setCurrency("euro");
 
 		lenient().when(accountRepository.findByAccountNumber("123456789")).thenReturn(one);
 		lenient().when(accountRepository.findByAccountNumber("987654321")).thenReturn(two);
-		lenient().when(bankNotesRepositotry.findAll()).thenReturn(list);
+		lenient().when(bankNotesRepository.findAll()).thenReturn(list);
+		try {
+			lenient().when(withdrawalService.dispense(200)).thenReturn(notes200);
+			lenient().when(withdrawalService.dispense(900)).thenReturn(notes900);
+			lenient().when(accountRepository.saveAndFlush(one)).thenReturn(account);
+
+		} catch (CommonServiceException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -87,7 +108,6 @@ public class WithdrawalServiceTest {
 			assertThat(recieved.getResponseMessage()).isEqualTo("invalid account number");
 			assertThat(recieved.getResponseStatus()).isEqualTo("failed");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -107,12 +127,11 @@ public class WithdrawalServiceTest {
 			assertThat(recieved.getResponseMessage()).isEqualTo("invalid pin");
 			assertThat(recieved.getResponseStatus()).isEqualTo("failed");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test failed withdrawal for Amount larger than maximum allowed amount")
 	void largerThanAccessibleAmount() {
@@ -128,12 +147,12 @@ public class WithdrawalServiceTest {
 			assertThat(recieved.getResponseMessage()).isEqualTo("amount is larger than maximum withdrawal amount");
 			assertThat(recieved.getResponseStatus()).isEqualTo("failed");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test failed withdrawal for amount not dispensible")
 	void failedWithdrawalForBadAmount() {
@@ -148,17 +167,57 @@ public class WithdrawalServiceTest {
 			assertThat(recieved.getResponseCode()).isNotEqualTo("0");
 			assertThat(recieved.getResponseStatus()).isEqualTo("failed");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	@DisplayName("Test successful withdrawal for amount less than balance")
+	void sucessfulWithdawalLessThanBalance() {
+
+		WithdrawalRequest request = new WithdrawalRequest();
+		request.setAccountNumber("123456789");
+		request.setPin("1234");
+		request.setAmount(200);
+
+		try {
+
+			WithdrawalResponse recieved = withdrawalService.withdraw(request);
+			assertThat(recieved.getResponseCode()).isEqualTo("0");
+			assertThat(recieved.getResponseStatus()).isEqualTo("approved");
+		} catch (CommonServiceException e) {
 			e.printStackTrace();
 		}
 
 	}
 	
 	@Test
+	@DisplayName("Test successful withdrawal for amount more than balance")
+	void sucessfulWithdawalMoreThanBalance() {
+
+		WithdrawalRequest request = new WithdrawalRequest();
+		request.setAccountNumber("123456789");
+		request.setPin("1234");
+		request.setAmount(900);
+
+		try {
+
+			WithdrawalResponse recieved = withdrawalService.withdraw(request);
+			assertThat(recieved.getResponseCode()).isEqualTo("0");
+			assertThat(recieved.getResponseStatus()).isEqualTo("approved");
+		} catch (CommonServiceException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
 	@DisplayName("Test failed dispense amount less than five")
 	void ShouldNotDispenseWhenAmountLessThanFive() {
 
-		double amount = 3.0 ;
+		double amount = 3.0;
 		BankNotes notes;
 
 		try {
@@ -167,17 +226,16 @@ public class WithdrawalServiceTest {
 			assertThat(notes.getResponseMessage()).isEqualTo("can't dispense amount less than 5 euros");
 			assertThat(notes.getResponseStatus()).isEqualTo("failed");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test failed dispense amount not divisible by five")
 	void ShouldNotDispenseWhenAmoutNotDivisibleByFive() {
 
-		double amount = 7 ;
+		double amount = 7;
 		BankNotes notes;
 
 		try {
@@ -186,17 +244,16 @@ public class WithdrawalServiceTest {
 			assertThat(notes.getResponseMessage()).isEqualTo("can't dispense amount not divisible by 5");
 			assertThat(notes.getResponseStatus()).isEqualTo("failed");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test successful dispensing amount with all notes")
 	void successfulDispensingAllNotes() {
 
-		double amount = 1305.0 ;
+		double amount = 1305.0;
 		BankNotes notes;
 
 		try {
@@ -205,17 +262,17 @@ public class WithdrawalServiceTest {
 			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
 			assertThat(notes.getResponseStatus()).isEqualTo("approved");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test failed when amount equals zero")
 	void shouldNotDispenseWhenAmountEqualsZero() {
 
-		double amount = 0.0 ;
+		double amount = 0.0;
 		BankNotes notes;
 
 		try {
@@ -224,36 +281,37 @@ public class WithdrawalServiceTest {
 			assertThat(notes.getResponseMessage()).isEqualTo("can't dispense amount less than 5 euros");
 			assertThat(notes.getResponseStatus()).isEqualTo("failed");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test successful dispensing less than number of 50 euros notes in the machine")
 	void shouldDispenseSuccessfulLessThan50() {
 
-		double amount = 200.0 ;
+		double amount = 200.0;
 		BankNotes notes;
 
 		try {
+
 			notes = withdrawalService.dispense(amount);
 			assertThat(notes.getResponseCode()).isEqualTo("0");
 			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
 			assertThat(notes.getResponseStatus()).isEqualTo("approved");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test successful dispensing less than number of 20 euros notes in the machine")
 	void shouldDispenseSuccessfulLessThan20() {
 
-		double amount = 40.0 ;
+		double amount = 40.0;
 		BankNotes notes;
 
 		try {
@@ -262,17 +320,17 @@ public class WithdrawalServiceTest {
 			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
 			assertThat(notes.getResponseStatus()).isEqualTo("approved");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test successful dispensing less than number of 10 euros notes in the machine")
 	void shouldDispenseSuccessfulLessThan10() {
 
-		double amount = 10.0 ;
+		double amount = 10.0;
 		BankNotes notes;
 
 		try {
@@ -281,17 +339,17 @@ public class WithdrawalServiceTest {
 			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
 			assertThat(notes.getResponseStatus()).isEqualTo("approved");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test successful dispensing less than number of 5 euros notes in the machine")
 	void shouldDispenseSuccessfulLessThan5() {
 
-		double amount = 5.0 ;
+		double amount = 5.0;
 		BankNotes notes;
 
 		try {
@@ -300,17 +358,17 @@ public class WithdrawalServiceTest {
 			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
 			assertThat(notes.getResponseStatus()).isEqualTo("approved");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test successful dispensing between 50 and 20")
 	void shouldDispenseSuccessfulBetween50and20() {
 
-		double amount = 1500.0 ;
+		double amount = 1500.0;
 		BankNotes notes;
 
 		try {
@@ -319,17 +377,17 @@ public class WithdrawalServiceTest {
 			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
 			assertThat(notes.getResponseStatus()).isEqualTo("approved");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test successful dispensing between 10 and 5")
 	void shouldDispenseSuccessfulBetween10and5() {
 
-		double amount = 1700.0 ;
+		double amount = 1700.0;
 		BankNotes notes;
 
 		try {
@@ -338,17 +396,17 @@ public class WithdrawalServiceTest {
 			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
 			assertThat(notes.getResponseStatus()).isEqualTo("approved");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	@Test
 	@DisplayName("Test failed no banknotes")
 	void shouldNotDispenseMachineIsEmpty() {
 
-		double amount = 1500.0 ;
+		double amount = 1500.0;
 		BankNotes notes;
 
 		try {
@@ -358,11 +416,10 @@ public class WithdrawalServiceTest {
 			assertThat(notes.getResponseMessage()).isEqualTo("successful dispensing");
 			assertThat(notes.getResponseStatus()).isEqualTo("approved");
 		} catch (CommonServiceException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 
 	}
-
 
 }
